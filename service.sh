9f890d8f2c54e5e3a70c73deb597750f2528baea
its up to everyone <40142806+danastyone@users.noreply.github.com>
2019-12-01 13:39:17 -0500

    Add files via upload

---
+++ service.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env sh
+#
+# Service.sh for Radarr
+
+# import DroboApps framework functions
+. /etc/service.subr
+
+framework_version="2.1"
+name="Radarr"
+version="0.2.0.1358"
+description="Radarr"
+depends=""
+webui=":7878"
+
+prog_dir="$(dirname "$(realpath "${0}")")"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
+statusfile="${tmp_dir}/status.txt"
+errorfile="${tmp_dir}/error.txt"
+
+# backwards compatibility
+if [ -z "${FRAMEWORK_VERSION:-}" ]; then
+  framework_version="2.0"
+  . "${prog_dir}/libexec/service.subr"
+fi
+
+start() {
+  rm -f "${errorfile}"
+  echo "Soanrr is running." > "${statusfile}"
+  touch "${pidfile}"
+  sudo ../mono/bin/mono /mnt/DroboFS/Shares/DroboApps/Radarr/Radarr.exe 
+  return 0
+}
+
+is_running() {
+  [ -f "${pidfile}" ]
+}
+
+stop() {
+  rm -f "${pidfile}"
+  return 0
+}
+
+force_stop() {
+  rm -f "${pidfile}"
+  return 0
+}
+
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+STDOUT=">&3"
+STDERR=">&4"
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o xtrace   # enable script tracing
+
+main "${@}"
